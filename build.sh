9501b19c33483b1835c261699d38cf2e08c7236a
George <george@omame.com>
2018-01-30 01:14:16 +0900

    first commit

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export FrameworkPathOverride=/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/4.6.1-api/
+
+if test "$OS" = "Windows_NT"
+then
+  # use .Net
+
+  .paket/paket.exe restore
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+  	exit $exit_code
+  fi
+
+  packages/build/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx
+else
+  # use mono
+  mono .paket/paket.exe restore
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+  	exit $exit_code
+  fi
+  mono packages/build/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
+fi
